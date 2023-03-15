9b108bd5fd19eb3679299921c85de23dc1ddb221
Mateus Martins S <108599241+UnlikeMaartin@users.noreply.github.com>
2023-03-14 22:42:00 -0300

    Add files via upload

--- AulaEstrutura.java
+++ AulaEstrutura.java
@@ -15,18 +15,48 @@ public class AulaEstrutura {
 
     public static void main(String[] args) {
         Scanner ler = new Scanner (System.in);
+        int numeros [] = new int[20];
+        double real [] = new double[5];
+        
+        /**
+         * Vetor de letras
+         */
         String letras [] = new String [10];
         
-        System.out.println("Digite uma letra");
+        System.out.println("Digite 10 letras letra");
             for(int i=0; i<letras.length;i++){
-                System.out.println("_");
-              
-              letras[i]= ler.nextLine();
+                System.out.print(i+1+"º letra:");
+                letras[i]=ler.nextLine();
         }
-        System.out.println("Números Informados: ");
+        System.out.println("As letras informadas foram: ");
             for(int i=0;i<letras.length;i++){
-              System.out.print("." + letras[i]);
-     }
+            System.out.print(" " + letras[i]);
+        }
+        /**
+         * Vetor de números
+         */        
+        System.out.println("Digite 20 números");
+            for(int i=0; i<numeros.length;i++){
+                System.out.println(i+1+"º número:");
+                numeros[i]=ler.nextInt();
+        }
+        System.out.println("Números informados: ");
+            for(int i=0;i<numeros.length;i++){
+                System.out.print(" /"+numeros[i]);
+        }
+        /**
+         * Vetor de valores do tipo double
+         */
+
+        System.out.println("Digite 5 números com valores quebras \nex:10.5\n");
+            for(int i=0; i<real.length;i++){
+                System.out.print(i+1+"º número:");
+                real[i]=ler.nextDouble();
+        }
+        System.out.println("Números informados");
+            for(int i=0;i<real.length;i++){
+                System.out.print(" /"+real[i]);
+            }
     }
     
 }
